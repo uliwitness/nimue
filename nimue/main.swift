ad12be07c57f2ce06397825aa969ca9a1faee325
Uli Kusterer <ulivc@zathras.de>
2020-01-25 02:04:24 +0100

    Initial Commit

---
+++ nimue/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  nimue
+//
+//  Created by Uli Kusterer on 25.01.20.
+//  Copyright © 2020 Uli Kusterer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
